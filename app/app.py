0e4180c36ea94b01f9bbb83a17fff1c5aced9681
Aidan Barton <bartonaidan6@gmail.com>
2021-10-30 20:35:50 +0000

    Inital Commit

---
+++ app/app.py
@@ -0,0 +1,20 @@
+from flask import Flask, render_template, redirect, url_for
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/gallery')
+def exper():
+    return render_template('gallery.html')
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+
+if __name__ == '__main__':
+    app.run()
